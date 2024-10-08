131fc56ad084c2e29ed08596bbacf0c170134ae4
eoghanriley <me@eoghanriley.dev>
2024-10-07 21:07:50 -0400

    Added userclass to admin page

--- ravel/contests/admin.py
+++ ravel/contests/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Problem, Contest, ContestProblem, Team, Submission, UserData
+from .models import Problem, Contest, ContestProblem, Team, Submission, UserData, UserClass
 
 # Register your models here.
 admin.site.register(Team)
@@ -9,3 +9,4 @@ admin.site.register(Contest)
 admin.site.register(ContestProblem)
 admin.site.register(Submission)
 admin.site.register(UserData)
+admin.site.register(UserClass)

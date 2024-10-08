919ef2cd81640a1ffaddb09af7a907228c88eb3f
eoghanriley <me@eoghanriley.dev>
2024-10-07 21:24:08 -0400

    Fixed reading env from settings

--- ravel/ravel/settings.py
+++ ravel/ravel/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS')).split(',')
 CSRF_TRUSTED_ORIGINS = str(os.getenv('CSRF_TRUSTED_ORIGINS')).split(',')
